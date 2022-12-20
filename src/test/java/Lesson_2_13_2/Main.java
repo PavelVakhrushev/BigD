cab2e0743c72f236d5ade80e67bba527dec33548
Pavel V <pavelv@gmail.com>
2022-12-20 13:30:29 +0400

    Add command and refresh

--- src/test/java/Lesson_2_13_2/Main.java
+++ src/test/java/Lesson_2_13_2/Main.java
@@ -1,4 +1,30 @@
 package Lesson_2_13_2;
 
 public class Main {
+    public static void main(String[] args) {
+        class DaysOfWeek {
+            final static String d1 = "Понедельник";
+            final static String d2 = "Вторник";
+            final static String d3 = "Среда";
+            final static String d4 = "Четверг";
+            final static String d5 = "Пятница";
+            final static String d6 = "Суббота";
+            final static String d7 = "Воскресенье";
+
+        }
+
+        class Day {
+            String dayOfWeek;
+            final int Date = 5;
+
+        }
+
+        class Week {
+            final int numberOfWeek1 = 49;
+            final int numberOfWeek2 = 50;
+            final int numberOfWeek3 = 51;
+
+        }
+        System.out.println("Неделя: ");
+    }
 }

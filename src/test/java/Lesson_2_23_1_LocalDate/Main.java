8c55b5f4803b2c7f756ab3d06fcc0b96583ed461
Pavel V <pavelv@gmail.com>
2023-01-20 17:06:25 +0400

    Задание с днем рождения

---
+++ src/test/java/Lesson_2_23_1_LocalDate/Main.java
@@ -0,0 +1,29 @@
+package Lesson_2_23_1_LocalDate;
+
+import java.time.*;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Main {
+    Instant timeStart = Instant.now();
+
+    Instant timeStartDate = Instant.now();
+    Date myBirthDayDate = new Date(85,07,23,05,17,03);//моя дата рождения класса Date
+
+    Instant timeStartGregorianCalendar = Instant.now();
+    GregorianCalendar myBirthDayGregorianCalendar =
+            new GregorianCalendar(1985,07,23,05,17,03);
+    // дата рождения класса GregorianCalendar
+
+    Instant timeStartLocalDate = Instant.now();
+    LocalDate myBirthDayLocalDate = new LocalDate.of(1985,07,23); // дата рождения класса LocalDate
+
+    Instant timeStartLocalDateTime = Instant.now();
+    LocalDateTime myBirthDayLocalDateTime = new LocalDateTime.of(1985,07,23,05,17,03,6564);
+    // дата рождения класса LocalDateTime
+
+    Instant timeStartZonedDateTime = Instant.now();
+    ZonedDateTime myBirthDayZonedDateTime =
+            new ZonedDateTime.of(1988, 07, 23, 05, 17, 03, 60, ZoneId.of("UTC + 4"));
+    // дата рождения класса ZonedDateTime
+}

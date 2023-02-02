c50d9a8a26989ec6e35e62dec6d17211fdfbe064
Pavel V <pavelv@gmail.com>
2023-02-02 12:07:36 +0400

    Добавил решение задания по 38 уроку

--- src/test/java/Lesson_2_38/Task_1.java
+++ src/test/java/Lesson_2_38/Task_1.java
@@ -1,4 +1,7 @@
 package Lesson_2_38;
+/* В программу через аргументы командной строки передаются три целых числа: a, b, c.
+Выведите наибольшее из данных чисел, (программа должна вывести ровно одно целое число).
+Проверку на наличие аргументов делать не надо.*/
 
 public class Task_1 {
     public static void main(String[] args) {
@@ -6,10 +9,10 @@ public class Task_1 {
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
 
-        if (a > b && a > c) {
+        if (a >= b && a >= c) {
             System.out.println(a);
 
-        } else if (b > a && b > c) {
+        } else if (b >= a && b >= c) {
             System.out.println(b);
 
         } else {

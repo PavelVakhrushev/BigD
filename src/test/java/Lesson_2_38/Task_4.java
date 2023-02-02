665abea0f419942d5927b7709863e0c78fe33851
Pavel V <pavelv@gmail.com>
2023-02-02 10:36:27 +0400

    Добавил решение задания по 38 уроку

--- src/test/java/Lesson_2_38/Task_4.java
+++ src/test/java/Lesson_2_38/Task_4.java
@@ -5,17 +5,21 @@ public class Task_4 {
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
         int c = Integer.parseInt(args[2]);
+        int maxSideTriangle, res;
 
-        if (a + b > c && c + b > a && a + c > b) {
-            System.out.println("impossible");
-            if (a * a + b * b == c * c || a * a + c * c == b * b || c * c + b * b == a * a) {
+        if (a < c + b && c < a + b && b < c + a) { // проверяем, возможен ли такой треугольник
+            if (a > b) maxSideTriangle = a; // находим наибольшую сторону у треугольника
+            else maxSideTriangle = b;
+            if (maxSideTriangle < c) maxSideTriangle = c;
+            res = a * a + b * b + c * c - 2 * (maxSideTriangle * maxSideTriangle); // считаем знак "+", "-" или "="
+            if (res > 0)
+            System.out.println("acute");
+            else if (res < 0)
+                System.out.println("obtuse");
+            else
                 System.out.println("right");
-            } else if (a * a + b * b < c * c || a * a + c * c < b * b || c * c + b * b < a * a) {
-                System.out.println("obtuse ");
-            } else if (a * a + b * b > c * c || a * a + c * c > b * b || c * c + b * b > a * a) {
-                System.out.println("acute ");
-
-            }
+        } else {
+            System.out.println("impossible");
         }
     }
 }

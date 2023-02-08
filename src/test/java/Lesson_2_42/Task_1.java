0372d78370268e371273d1e97181f980aa731cb2
PavelVakhrushev <vapami69@gmail.com>
2023-02-08 17:55:46 +0400

    Задания по уроку 42

--- src/test/java/Lesson_2_42/Task_1.java
+++ src/test/java/Lesson_2_42/Task_1.java
@@ -7,4 +7,14 @@ package Lesson_2_42;
 Проверку на наличие аргументов делать не надо.*/
 
 public class Task_1 {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+        for (int x = m; x <= n; x++) {
+            if (x % 3 == 0 && x % 5 != 0) {
+                System.out.println("X = " + x);
+            }
+        }
+    }
 }

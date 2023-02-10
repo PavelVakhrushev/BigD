826ed28a3794bba265b669f3105a8e5f9e431391
PavelVakhrushev <vapami69@gmail.com>
2023-02-11 00:43:31 +0400

    Задания по уроку 42

--- src/test/java/Lesson_2_42/Task_3.java
+++ src/test/java/Lesson_2_42/Task_3.java
@@ -1,5 +1,4 @@
 package Lesson_2_42;
-
 /* Напишите метод snakePrint(int n, int col),
 который выводит числа "змейкой" от 1 до n:
     1 2 3 4
@@ -10,15 +9,46 @@ package Lesson_2_42;
 n - максимальное число
 col - количество цифр в одной строке
 Выводимые числа разделены пробелами.
-
 Используя реализованный метод, напишите программу, которая через
 аргументы командной строки получает целочисленные значения n и col,
 а затем печатает соответствующего размера змейку.
-
 Проверку на наличие аргументов делать не надо.
 Сделайте проверку, что n больше или равно 1, а m больше 0.
 В противном случае выведите сообщение об ошибке
 и завершите программу. */
 
 public class Task_3 {
+
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int col = Integer.parseInt(args[1]);
+        snakePrint(n, col);
+    }
+
+    public static void snakePrint(int n, int col) {
+        if (n > 1 && col > 0) {
+            int count = 0;
+            for (int i = 1; i <= n; i++) {
+                System.out.print(i + " ");
+                count++;
+                if (count == col) {
+                    System.out.println();
+                    count = 0;
+                    break;
+                }
+            }
+            for (int k = n; k > 0; k-- ) {
+                System.out.print(k + " ");
+                count++;
+                if (count == col) {
+                    System.out.println();
+                    count = 0;
+                    break;
+                }
+            }
+        } else {
+            System.out.println("ERROR");
+        }
+    }
+
 }

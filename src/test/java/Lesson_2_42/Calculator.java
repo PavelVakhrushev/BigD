0384cc6fda52bfed2da7cd4f33d7f722252ea4a6
PavelVakhrushev <vapami69@gmail.com>
2023-02-15 22:00:56 +0400

    Задания по уроку 42

--- src/test/java/Lesson_2_42/Calculator.java
+++ src/test/java/Lesson_2_42/Calculator.java
@@ -9,7 +9,6 @@ package Lesson_2_42;
   Если равно, то пишется сообщение, что операция не может быть выполнена,
   и программа завершается, используя System.exit(0);
 
-
 Используя класс Calculator, напишите программу, которая будет
 получать через аргументы командной строки два целых числа,
 знак операции и после этого выводить результат операции.
@@ -26,5 +25,43 @@ package Lesson_2_42;
 Если введена неподдерживаемая операция,
 то выводить список поддерживаемых операций и завершать программу.*/
 
-public class Task_5 {
+public class Calculator {
+    static int add(int x, int y) {   // выполнение x + y
+        return x + y;
+    }
+    static int sub(int x, int y) {  // выполнение x - y
+        return x - y;
+    }
+    static int mult(int x, int y) {// выполнение x * y
+        return x * y;
+    }
+    static int divide(int x, int y) {// выполнение x / y.
+        return x / y;
+    }
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        String b = args[1];
+        int y = Integer.parseInt(args[2]);
+        if (y == 0) {
+            System.out.println("Операция не может быть выполнена ");
+            System.exit(0);
+        }
+        switch (b) {
+            case ("+"):
+                System.out.println(x + " " + b + " " + y + " = " + add(x, y));
+                break;
+            case ("-"):
+                System.out.println(x + " " + b + " " + y + " = " + sub(x, y));
+                break;
+            case ("*"):
+                System.out.println(x + " " + b + " " + y + " = " + mult(x, y));
+                break;
+            case ("/"):
+                System.out.println(x + " " + b + " " + y + " = " + divide(x, y));
+                break;
+            default:
+                System.out.println("Программа может только +, -, *, /");
+                break;
+        }
+    }
 }

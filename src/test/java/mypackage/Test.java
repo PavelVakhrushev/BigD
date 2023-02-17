11851138f21221c87ee4ffd8f8fb96105e1d6941
PavelVakhrushev <vapami69@gmail.com>
2023-02-17 19:45:45 +0400

    Задания по уроку 48. String.

--- src/test/java/mypackage/Test.java
+++ src/test/java/mypackage/Test.java
@@ -1,18 +1,5 @@
 package mypackage;
 
 class Test {
-    public static void main(String[] args) {
-
-        int x = 2;
-        switch(x) {
-            case 1, 2, 3:
-                System.out.println();
-                break;
-            case 4, 5, 6:
-                System.out.println();
-                break;
-        }
-        System.out.println(x);
-    }
 
 }

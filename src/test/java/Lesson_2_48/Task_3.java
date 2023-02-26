76151289af2cc27b7ae24f50452afb58f6c665a2
PavelVakhrushev <vapami69@gmail.com>
2023-02-26 21:59:05 +0400

    Задания по уроку 48. Откорретировал упражнение 3.

--- src/test/java/Lesson_2_48/Task_3.java
+++ src/test/java/Lesson_2_48/Task_3.java
@@ -10,14 +10,26 @@ package Lesson_2_48;
 Вывод: 10a01vt1010c
 Проверку на наличие и корректность аргумента делать не надо.*/
 public class Task_3 {
-    public static void main(String[] args) {
-        String str = String.valueOf(args[0]);
-        StringBuilder sb = new StringBuilder(str);
-        sb.replace(0, sb.length(), sb.toString().replace("0", "temp")
-                .replace("1", "0").replace("temp", "1"));
 
-            System.out.println(sb);
+    static String str;
+
+    public static void replace() {
+        StringBuilder sb = new StringBuilder(str);
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c == '0') {
+                sb.setCharAt(i, '1');
+            } else if (c == '1') {
+                sb.setCharAt(i, '0');
+            }
         }
+        System.out.println(sb);
     }
+
+    public static void main(String[] args) {
+        str = args[0];
+        replace();
+    }
+}
 
 

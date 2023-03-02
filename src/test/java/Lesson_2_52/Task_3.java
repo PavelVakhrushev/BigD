3610313e4e7a1ae340298974da872f5d77ef1b3f
PavelVakhrushev <vapami69@gmail.com>
2023-03-02 23:27:26 +0400

    Задания по уроку 52. Упражнение 3.

--- src/test/java/Lesson_2_52/Task_3.java
+++ src/test/java/Lesson_2_52/Task_3.java
@@ -4,22 +4,46 @@ package Lesson_2_52;
 Массив должен содержать не более 5 строк и каждая строка иметь не более 7 столбцов.
 Выведите массив на экран в порядке убывания сумм элементов строк.*/
 
+import java.util.Arrays;
+
 public class Task_3 {
 
     static void array() {
-        int[][] a = new int[5][7];
-        for (int i = 0; i < a.length ; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                a[i][j] = (int) (Math.random() * 19) - 9;
-                System.out.print(a[i][j] + " ");
+        int[][] arr = new int[5][7];
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = (int) (Math.random() * 19) - 9;
+
+                //System.out.print(arr[i][j] + " ");
             }
-            System.out.println();
+            //System.out.println();
         }
+        sortArray(arr);
+
+        for (int[] ints : arr) {
+            System.out.println(Arrays.toString(ints));
+        }
+
+    }
+
+
+    public static int getSum(int[] array) {
+        int sum = 0;
+        for (int j : array) {
+            sum += j;
+        }
+        return sum;
+    }
+
+    public static void sortArray (int[][] arr) {
+        Arrays.sort(arr, (a, b) -> {
+            int x = getSum(b);
+            int y = getSum(a);
+            return Integer.compare(x, y);
+        });
     }
 
     public static void main(String[] args) {
         array();
-
-
     }
 }

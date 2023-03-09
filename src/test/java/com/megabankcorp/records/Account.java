a8a477a8447416b732db94d92eaed9531598c044
PavelVakhrushev <vapami69@gmail.com>
2023-03-09 21:40:53 +0400

    Задания по уроку 58. Упражнение 1.

---
+++ src/test/java/com/megabankcorp/records/Account.java
@@ -0,0 +1,9 @@
+package com.megabankcorp.records;
+
+import java.math.BigDecimal;
+
+public class Account {
+    BigDecimal sum;
+    long numAcc;
+    long namClient;
+}
--- src/test/java/mypackage/cars/Car.java
+++ src/test/java/mypackage/cars/Car.java
@@ -1,7 +1,21 @@
 package mypackage.cars;
 
 public class Car {
-    public static void Move () {
-
+    public static void main(String[] args) {
+        int w = 0, r = 1;
+        String name = "";
+        while(w < 2) {
+            name += "A";
+            do {
+                name += "B";
+                if (name.length() > 0)
+                    name += "C";
+                else
+                    break;
+            } while (r <= 1);
+            r++;
+            w++;
+            System.out.println(name);
+        }
     }
 }

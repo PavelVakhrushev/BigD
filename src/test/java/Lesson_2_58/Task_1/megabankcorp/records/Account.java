8a701e2d2535ea7334e1e76366bc73aa84ad043c
PavelVakhrushev <vapami69@gmail.com>
2023-03-13 23:04:38 +0400

    Задания по уроку 58. Упражнение 1.

--- src/test/java/Lesson_2_58/Task_1/megabankcorp/records/Account.java
+++ src/test/java/Lesson_2_58/Task_1/megabankcorp/records/Account.java
@@ -3,9 +3,9 @@ package Lesson_2_58.Task_1.megabankcorp.records;
 import java.math.BigDecimal;
 
 public class Account {
-    private BigDecimal balance; // поле для текущей суммы на счете
-    private long currentAccount; // поле для текущего номера счета
-    private long idClient; // поле для номера клиента банка
+    private final BigDecimal balance; // поле для текущей суммы на счете
+    private final long currentAccount; // поле для текущего номера счета
+    private final long idClient; // поле для номера клиента банка
 
     public long getIdClient() {  // получаем номер клиента банка
         return idClient;
@@ -18,6 +18,12 @@ public class Account {
 
     public long getCurrentAccount() { // получаем текущий номер счета клиента банка
         return currentAccount;
+    }
+
+    public Account(BigDecimal balance, long currentAccount, long idClient) {
+        this.balance = balance;
+        this.currentAccount = currentAccount;
+        this.idClient = idClient;
     }
 
 }
